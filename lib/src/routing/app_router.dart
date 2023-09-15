c6732a76e519cd29f6d91585b41341efec99f83f
Kendi J <kendij@Kendis-MacBook-Pro.local>
2023-09-16 01:37:56 +0300

    Hipe it works🔥

---
+++ lib/src/routing/app_router.dart
@@ -0,0 +1,43 @@
+import 'package:faker_app_flutter_firebase/src/screens/custom_profile_screen.dart';
+import 'package:faker_app_flutter_firebase/src/screens/custom_sign_in_screen.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:go_router/go_router.dart';
+
+enum AppRoute {
+  signIn,
+  profile,
+}
+
+final goRouterProvider = Provider<GoRouter>((ref) {
+  return GoRouter(
+    initialLocation: '/sign-in',
+    debugLogDiagnostics: true,
+    redirect: (context, state) {
+      final isLoggedIn = FirebaseAuth.instance.currentUser != null;
+      if (isLoggedIn) {
+        if (state.uri.path == '/sign-in') {
+          return '/profile';
+        }
+      } else {
+        if (state.uri.path == '/profile') {
+          return '/sign-in';
+        }
+      }
+      return null;
+      
+    },
+    routes: [
+      GoRoute(
+        path: '/sign-in',
+        name: AppRoute.signIn.name,
+        builder: (context, state) => const CustomSignInScreen(),
+      ),
+      GoRoute(
+        path: '/profile',
+        name: AppRoute.profile.name,
+        builder: (context, state) => const CustomProfileScreen(),
+      ),
+    ],
+  );
+});
